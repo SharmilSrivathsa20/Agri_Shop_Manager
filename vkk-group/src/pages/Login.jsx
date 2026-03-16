8d5f360401dde6abff09c7eba573d18f2f05ef0d
SharmilSrivathsa20 <srivathsa.sharmil@gmail.com>
2026-03-16 15:24:23 +0530

    fix: stable vite dependency

--- vkk-group/src/pages/Login.jsx
+++ vkk-group/src/pages/Login.jsx
@@ -42,7 +42,7 @@ function Login() {
       <div className="bg-white p-10 rounded-xl shadow-xl w-96">
 
         <img
-          src="/VKK.png"
+          src="../VKK.png"
           alt="VKK Logo"
           className="w-24 mx-auto mb-4"
         />
